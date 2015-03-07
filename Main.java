0d750b42c27d660ed4e8b4e87fa49f5c2ea4c54d
Jardel Anton <jardellanton@gmail.com>
2015-03-07 12:32:19 -0300

    implementação dos atributos do retangulo e teste com main

---
+++ Main.java
@@ -0,0 +1,9 @@
+class main{
+	public static void main(String args[]){
+	Retangulo r1;
+	r1=new Retangulo();
+	r1.width=9;
+	r1.height=10;
+	r1.oi();
+}
+}
--- Retangulo.java
+++ Retangulo.java
@@ -2,5 +2,8 @@ class Retangulo{
 	public int height;
 	public int width;
 
-	
+	public void oi(){
+		System.out.println("oi"+this.width+" X "+this.height);
+
+	}
 }
