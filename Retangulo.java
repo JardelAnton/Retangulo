ae98eaabca9c5c32c1f5820075639756679137c4
Jardel Anton <jardellanton@gmail.com>
2015-03-07 12:51:49 -0300

    implementação do calculo de area e perimetro

--- Retangulo.java
+++ Retangulo.java
@@ -2,8 +2,9 @@ class Retangulo{
 	public int height;
 	public int width;
 
-	public void oi(){
-		System.out.println("oi"+this.width+" X "+this.height);
+	public void oi(){	
+		System.out.println("Area = "+this.width*this.height);
+		System.out.println("Perimetro = "+((2*this.width)+(2*this.height)));
 
 	}
 }
