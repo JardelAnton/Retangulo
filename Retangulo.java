0b72ea0d35efebda8e3a336b036e6c700066f6ee
Jardel Anton <jardellanton@gmail.com>
2015-03-08 20:36:25 -0300

    colocando unidades

--- Retangulo.java
+++ Retangulo.java
@@ -3,9 +3,9 @@ class Retangulo{
 	public int width;
 
 	public void area(){
-		System.out.println("Area = "+this.width*this.height+"u.c²");
+		System.out.println("Area = "+this.width*this.height+"u.a²");
 	}
 	public void perimetro(){
-		System.out.println("Perimetro = "+((2*this.width)+(2*this.height)));
+		System.out.println("Perimetro = "+(2*this.width+2*this.height)+"u.c");
 	}
 }
