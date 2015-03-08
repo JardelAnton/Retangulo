647d76dfa277c3f8f1d3d70eaa56df125a43c5a5
Jardel Anton <jardellanton@gmail.com>
2015-03-08 19:55:52 -0300

    Finalização da class retangulo

--- Retangulo.java
+++ Retangulo.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
 class Retangulo{
 	public int height;
 	public int width;
 
-	public void area(){	
+	public void area(){
 		System.out.println("Area = "+this.width*this.height);
 	}
 	public void perimetro(){
 		System.out.println("Perimetro = "+((2*this.width)+(2*this.height)));
-		/*
-		Scanner entrada = new Scanner (System.in);
-		System.out.println("Digite seu nome: ");
-		String nome = entrada.nextLine();
-		System.out.println("O nome digitado foi: " + nome);*/
 	}
 }
